051bca9cdc6b03c22de274fca6861e6b711db696
hirasawa-flarenest <hirasawa_haruki@flarenest.co.jp>
2025-10-24 00:32:11 +0900

    ssg対応

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  output: "export", // ← 完全SSGにする
+  images: { unoptimized: true }, // next/imageの最適化を無効化（SSR依存のため）
+  // trailingSlash: true,     // 404回避で有効にするケースも。必要に応じてON
 };
-
 module.exports = nextConfig;
